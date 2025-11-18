614dfdb0e5e42f294d514199446fa31fadd5bc52
Kevin Vargas <vargaskevv@gmail.com>
2025-11-18 17:25:42 -0400

    feat: add tailwind and shadcn svelte

--- arukaweb/src/routes/+page.svelte
+++ arukaweb/src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
+<script lang="ts">
+    import { Button } from "$lib/components/ui/button";
+</script>
+
+<h1 class="text-center">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<Button>Click me</Button>
