9e68d1d368288b597705a05f32e483171b7e4479
Kevin <vargaskevv@gmail.com>
2025-12-31 22:14:34 -0400

    fix: chat message auto-scroll

--- arukaweb/src/features/chats/data/use-chat-messages.ts
+++ arukaweb/src/features/chats/data/use-chat-messages.ts
@@ -1,8 +1,12 @@
 import { chatClient } from "@common/utils/clients";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { chatQueryKeys } from "./chat-query-keys";
+import type { ChatMessagesResponse } from "@connect/chat/v1/chat_messages_pb";
+import type { Message } from "@connect/models/v1/message_pb";
 
 export function useChatMessages(chatId: string) {
+  const queryClient = useQueryClient();
+
   const query = useQuery({
     queryFn: () => chatClient.chatMessages({ chatId }),
     queryKey: chatQueryKeys.chatMessages(chatId),
@@ -10,8 +14,27 @@ export function useChatMessages(chatId: string) {
 
   const messages = query.data?.messages || [];
 
+  const addMessageToCache = (message: Message) => {
+    queryClient.setQueryData(
+      chatQueryKeys.chatMessages(chatId),
+      (prev: ChatMessagesResponse | undefined): ChatMessagesResponse => {
+        if (prev === undefined) {
+          return {
+            $typeName: "chat.v1.ChatMessagesResponse",
+            messages: [message],
+          };
+        }
+        return {
+          ...prev,
+          messages: [...prev.messages, message],
+        };
+      },
+    );
+  };
+
   return {
     messages,
+    addMessageToCache,
     loadingMessages: query.isLoading,
     fetchingMessages: query.isFetching,
     isFetchedMessages: query.isFetched,
--- arukaweb/src/features/chats/screen/Chat/Chat.tsx
+++ arukaweb/src/features/chats/screen/Chat/Chat.tsx
@@ -10,7 +10,9 @@ import { useFirstMessageStore } from "@features/chats/store/useFirstMessageStore
 import { createTextMessage } from "@features/chats/utils/create_text_message";
 import {
   ActionIcon,
+  Alert,
   Box,
+  Button,
   Card,
   Container,
   Flex,
@@ -18,13 +20,19 @@ import {
   Textarea,
 } from "@mantine/core";
 import { useForm } from "@mantine/form";
-import { useQueryClient } from "@tanstack/react-query";
 import { Fragment, useEffect, useLayoutEffect, useRef, useState } from "react";
 import styles from "./styles.module.css";
 import { INPUT_BOTTOM_DISTANCE } from "./utils";
 import { useRouter } from "@tanstack/react-router";
 
-export const chatsScrollMap: Record<string, number> = {};
+const ScrollType = {
+  LastMessage: "LastMessage",
+  NewMessage: "NewMessage",
+  RestoreChatPosition: "RestoreChatPosition",
+} as const;
+type ScrollType = (typeof ScrollType)[keyof typeof ScrollType];
+
+const chatsScrollMap = new Map<string, number>();
 
 interface Props {
   chatId: string;
@@ -33,15 +41,17 @@ interface Props {
 export function Chat(props: Props) {
   const { chatId } = props;
   const router = useRouter();
-  const queryClient = useQueryClient();
   const inputContainerRef = useRef<HTMLDivElement>(null);
   const messagesRef = useRef<HTMLDivElement>(null);
   const containerRef = useRef<HTMLDivElement>(null);
   const { firstMessage, clearFirstMessage } = useFirstMessageStore();
-  const { messages, isFetchedMessages } = useChatMessages(chatId);
+  const { messages, addMessageToCache, isFetchedMessages } =
+    useChatMessages(chatId);
   const [generatedResponse, setGeneratedResponse] = useState<Message | null>(
     null,
   );
+  const [responseError, setResponseError] = useState<string | null>(null);
+  const [scrollType, setScrollType] = useState<ScrollType | null>(null);
 
   const form = useForm({
     initialValues: { userText: firstMessage || "" },
@@ -50,53 +60,47 @@ export function Chat(props: Props) {
     },
   });
 
-  const newMessageScroll = () => {
-    if (!messagesRef.current || !containerRef.current) return;
-    containerRef.current.scrollTo({
-      top: messagesRef.current.scrollHeight,
-      behavior: "instant",
-    });
+  const retry = () => {
+    if (messages.length <= 0) return;
+    if (!responseError) return;
+    const lastMessage = messages[messages.length - 1];
+    const messageStr = lastMessage.content
+      .filter((c) => c.content.case === "textContent")
+      .map((c) => c.content.value?.text ?? "")
+      .join("");
+    setResponseError(null);
+    handleNewMessage(messageStr);
   };
 
   const handleNewMessage = async (message: string) => {
-    const res = chatClient.chatMessage({
-      chatId,
-      content: [{ content: { case: "textContent", value: { text: message } } }],
-    });
-    let responseText = "";
-    for await (const delta of res) {
-      responseText += delta.delta;
-      setGeneratedResponse(
-        createTextMessage(responseText, MessageRole.ASSISTANT),
-      );
+    try {
+      const res = chatClient.chatMessage({
+        chatId,
+        content: [
+          { content: { case: "textContent", value: { text: message } } },
+        ],
+      });
+      let responseText = "";
+      for await (const delta of res) {
+        responseText += delta.delta;
+        setGeneratedResponse(
+          createTextMessage(responseText, MessageRole.ASSISTANT),
+        );
+      }
+    } catch (e) {
+      setResponseError((e as Error)?.message ?? "Something went wrong");
     }
   };
 
   const handleSubmit = (values: typeof form.values) => {
-    queryClient.setQueryData(
-      chatQueryKeys.chatMessages(chatId),
-      (prev: ChatMessagesResponse | undefined): ChatMessagesResponse => {
-        if (prev === undefined) {
-          return {
-            $typeName: "chat.v1.ChatMessagesResponse",
-            messages: [createTextMessage(values.userText, MessageRole.USER)],
-          };
-        }
-        const newMessages = [...prev.messages];
-        if (generatedResponse) {
-          newMessages.push(generatedResponse);
-          setGeneratedResponse(null);
-        }
-        newMessages.push(createTextMessage(values.userText, MessageRole.USER));
-        return {
-          ...prev,
-          messages: newMessages,
-        };
-      },
-    );
-    newMessageScroll();
+    addMessageToCache(createTextMessage(values.userText, MessageRole.USER));
+    if (generatedResponse) {
+      addMessageToCache(generatedResponse);
+      setGeneratedResponse(null);
+    }
     handleNewMessage(values.userText);
     form.setValues({ userText: "" });
+    setScrollType(ScrollType.NewMessage);
   };
 
   // Auto-send message when we're comming from a /new chat
@@ -113,71 +117,71 @@ export function Chat(props: Props) {
   ]);
 
   // Auto-scroll to saved position when changing chats
-  useLayoutEffect(() => {
-    if (!(chatId in chatsScrollMap)) return;
-    if (!containerRef.current) return;
-    containerRef.current.scrollTo({
-      top: chatsScrollMap[chatId],
-      behavior: "instant",
-    });
+  useEffect(() => {
+    if (!chatsScrollMap.has(chatId)) return;
+    console.log("Effect:", ScrollType.RestoreChatPosition);
+    setScrollType(ScrollType.RestoreChatPosition);
   }, [chatId]);
 
   // Auto-scroll to bottom on initial chat load
-  useLayoutEffect(() => {
+  useEffect(() => {
     if (!isFetchedMessages) return;
-    if (
-      !containerRef.current ||
-      !messagesRef.current ||
-      !inputContainerRef.current
-    )
-      return;
-    if (chatId in chatsScrollMap) return;
-    containerRef.current.scrollTo({
-      top:
-        messagesRef.current.scrollHeight -
-        window.innerHeight +
-        inputContainerRef.current.clientHeight +
-        INPUT_BOTTOM_DISTANCE +
-        12,
-      behavior: "instant",
-    });
-  }, [chatId, isFetchedMessages]);
+    if (chatsScrollMap.has(chatId)) return;
+    if (messages.length <= 1) return; // 1 means first user message
+    console.log("Effect:", ScrollType.LastMessage);
+    setScrollType(ScrollType.LastMessage);
+  }, [chatId, isFetchedMessages, messages]);
 
   // Save last chat to query cache
   useEffect(() => {
     const unsubscribe = router.subscribe("onBeforeNavigate", () => {
       if (generatedResponse === null) return;
-      queryClient.setQueryData(
-        chatQueryKeys.chatMessages(chatId),
-        (
-          prev: ChatMessagesResponse | undefined,
-        ): ChatMessagesResponse | undefined => {
-          if (prev === undefined) return undefined;
-          return {
-            ...prev,
-            messages: [...prev.messages, generatedResponse],
-          };
-        },
-      );
+      addMessageToCache(generatedResponse);
       setGeneratedResponse(null);
     });
     return unsubscribe;
-  }, [chatId, generatedResponse]);
+  }, [chatId, generatedResponse, addMessageToCache]);
 
   // Listen to scroll changes and save it to restore position later
   useEffect(() => {
     const el = containerRef.current;
     if (!el) return;
-    const handleScrollEnd = () => {
-      chatsScrollMap[chatId] = el.scrollTop;
-    };
+    const handleScrollEnd = () => chatsScrollMap.set(chatId, el.scrollTop);
     el.addEventListener("scrollend", handleScrollEnd);
     return () => el.removeEventListener("scrollend", handleScrollEnd);
   }, [chatId]);
 
+  useLayoutEffect(() => {
+    if (!scrollType) return;
+    let top = 0;
+    switch (scrollType) {
+      case ScrollType.NewMessage:
+        top = messagesRef.current?.scrollHeight ?? 0;
+        top = top === 0 ? top : top - 1; // There are issues with exact measurement
+        break;
+      case ScrollType.RestoreChatPosition:
+        top = chatsScrollMap.get(chatId) ?? 0;
+        break;
+      case ScrollType.LastMessage:
+        top =
+          (messagesRef.current?.scrollHeight ?? 0) -
+          window.innerHeight +
+          (inputContainerRef.current?.clientHeight ?? 0) +
+          INPUT_BOTTOM_DISTANCE +
+          12;
+        top = Math.max(top, 0);
+    }
+    console.log("scrolling to: ", scrollType, top);
+    containerRef.current?.scrollTo({
+      top: top,
+      behavior: "instant",
+    });
+    setScrollType(null);
+  }, [scrollType]);
+
   return (
-    <Box px="md" className={styles.container} ref={containerRef}>
-      <Flex ref={messagesRef} direction="column" gap="lg" pt="xl">
+    <Box px="md" className={styles.container} ref={containerRef} id="ccc">
+    <Flex ref={messagesRef} direction="column" gap="lg" pt="xl">
         {messages.map((message) => (
           <Fragment key={message.id}>
             {message.role === MessageRole.USER && (
@@ -189,9 +193,26 @@ export function Chat(props: Props) {
           </Fragment>
         ))}
       </Flex>
-      <Box mih="100dvh">
+      <Box mih="110dvh" mt="xl">
         {generatedResponse && (
           <AIMessage content={generatedResponse?.content} />
+        )}
+        {responseError && (
+          <Container size="md">
+            <Alert color="red" title="Response error">
+              {responseError}
+              <Flex justify="flex-end" pb="xs">
+                <Button
+                  variant="outline"
+                  size="sm"
+                  color="gray"
+                  onClick={retry}
+                >
+                  Retry
+                </Button>
+              </Flex>
+            </Alert>
+          </Container>
         )}
         <Box h={120} />
       </Box>
