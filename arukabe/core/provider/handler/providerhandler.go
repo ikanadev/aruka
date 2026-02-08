1667935c6d806d6ace9371fa0fb75bc6b3adfe46
Kevin Vargas <vargaskevv@gmail.com>
2026-02-08 12:39:34 -0400

    fix: provider types

--- arukabe/core/provider/handler/providerhandler.go
+++ arukabe/core/provider/handler/providerhandler.go
@@ -4,8 +4,6 @@ import (
 	"arukabe/core/provider/service"
 	providerv1 "arukabe/gen/connect/provider/v1"
 	"context"
-
-	"connectrpc.com/connect"
 )
 
 func NewProviderHandler(service *service.ProviderService) *ProviderHandler {
@@ -19,23 +17,23 @@ type ProviderHandler struct {
 // ListProviders implements providerv1connect.ProviderServiceHandler.
 func (ph *ProviderHandler) ListProviders(
 	ctx context.Context,
-	req *connect.Request[providerv1.ListProvidersRequest],
-) (*connect.Response[providerv1.ListProvidersResponse], error) {
-	resp, err := ph.service.ListProviders(ctx, req.Msg)
+	req *providerv1.ListProvidersRequest,
+) (*providerv1.ListProvidersResponse, error) {
+	resp, err := ph.service.ListProviders(ctx, req)
 	if err != nil {
 		return nil, err
 	}
-	return connect.NewResponse(resp), nil
+	return resp, nil
 }
 
 // UpdateProviderModels implements providerv1connect.ProviderServiceHandler.
 func (ph *ProviderHandler) UpdateProviderModels(
 	ctx context.Context,
-	req *connect.Request[providerv1.UpdateProviderModelsRequest],
-) (*connect.Response[providerv1.UpdateProviderModelsResponse], error) {
-	resp, err := ph.service.UpdateProviderModels(ctx, req.Msg)
+	req *providerv1.UpdateProviderModelsRequest,
+) (*providerv1.UpdateProviderModelsResponse, error) {
+	resp, err := ph.service.UpdateProviderModels(ctx, req)
 	if err != nil {
 		return nil, err
 	}
-	return connect.NewResponse(resp), nil
+	return resp, nil
 }
